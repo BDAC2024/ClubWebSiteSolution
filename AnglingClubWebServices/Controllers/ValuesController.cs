0a939738f07f1a3e6669695d210233ceecbef635
Steve Townend <steve@townendmail.co.uk>
2021-04-12 19:26:44 +0100

    Add project files.

---
+++ AnglingClubWebServices/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BDAC.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnglingClubWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var repo = new WaterRepository();
+
+            repo.AddOrUpdateWater().Wait();
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BDAC.Repository/WaterRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+
+namespace BDAC.Repository
+{
+    public class WaterRepository : RepositoryBase
+    {
+
+        public WaterRepository(): base()
+        {
+        }
+
+        public async Task AddOrUpdateWater()
+        {
+            var client = GetClient();
+
+            BatchPutAttributesRequest request = new BatchPutAttributesRequest();
+            request.DomainName = DOMAIN;
+            request.Items.Add(
+                new ReplaceableItem
+                {
+                    Name = "Roecliffe Pond",
+                    Attributes = new List<ReplaceableAttribute>
+                    {
+                        new ReplaceableAttribute { Name = "Type", Value = "Water", Replace = false },
+                        new ReplaceableAttribute { Name = "Id", Value = "1", Replace = false },
+                        new ReplaceableAttribute { Name = "AccessType", Value = "MembersAndGuests", Replace = false },
+                        new ReplaceableAttribute { Name = "WaterType", Value = "Stillwater", Replace = false },
+                    }
+                }
+            );
+
+            try
+            {
+                BatchPutAttributesResponse response = await client.BatchPutAttributesAsync(request);
+                Console.WriteLine($"Water updated");
+            }
+            catch (AmazonSimpleDBException ex)
+            {
+                Console.WriteLine($"Error Code: {ex.ErrorCode}, Error Type: {ex.ErrorType}");
+                throw;
+            }
+
+        }
+
+            
+    }
+}
