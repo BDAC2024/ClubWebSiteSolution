eb2ec2d287ab4492d3e79689c203a0c76ee4e12f
Steve Townend <steve@townendmail.co.uk>
2021-05-05 18:23:32 +0100

    Add WarmingLambda feature

--- AnglingClubWebServices/LambdaEntryPoint.cs
+++ AnglingClubWebServices/LambdaEntryPoint.cs
@@ -1,8 +1,10 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
 namespace AnglingClubWebServices
 {
@@ -47,6 +49,183 @@ namespace AnglingClubWebServices
         /// <param name="builder"></param>
         protected override void Init(IHostBuilder builder)
         {
+        }
+
+        private static readonly DateTime LambdaTime = DateTime.Now;
+
+        // Note that this can be extracted by looking in cloudwatch after calling the api/healtcheck from postman and copying the response from there
+        private string realReq = @"
+        {
+            'Resource': '/{proxy+}',
+            'Path': '/api/healthcheck',
+            'HttpMethod': 'GET',
+            'Headers': {
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Cache-Control': 'no-cache',
+                'CloudFront-Forwarded-Proto': 'https',
+                'CloudFront-Is-Desktop-Viewer': 'true',
+                'CloudFront-Is-Mobile-Viewer': 'false',
+                'CloudFront-Is-SmartTV-Viewer': 'false',
+                'CloudFront-Is-Tablet-Viewer': 'false',
+                'CloudFront-Viewer-Country': 'GB',
+                'Host': 't5nynu5k43.execute-api.eu-west-1.amazonaws.com',
+                'Postman-Token': '9b5ee25b-95ad-4a89-93ef-711fabbda104',
+                'User-Agent': 'PostmanRuntime/7.28.0',
+                'Via': '1.1 1064639c622430d6b0382968293fd56e.cloudfront.net (CloudFront)',
+                'X-Amz-Cf-Id': '-hwcXYIM6mTExEuibdTST-S2nOe_FVKZQWVM32_p4u8OxqW7nj8h_g==',
+                'X-Amzn-Trace-Id': 'Root=1-6092cfea-75dc6e145a8d260d55b45e61',
+                'X-Forwarded-For': '81.153.217.128, 130.176.96.147',
+                'X-Forwarded-Port': '443',
+                'X-Forwarded-Proto': 'https'
+            },
+            'MultiValueHeaders': {
+                'Accept': [
+                    '*/*'
+                ],
+                'Accept-Encoding': [
+                    'gzip, deflate, br'
+                ],
+                'Cache-Control': [
+                    'no-cache'
+                ],
+                'CloudFront-Forwarded-Proto': [
+                    'https'
+                ],
+                'CloudFront-Is-Desktop-Viewer': [
+                    'true'
+                ],
+                'CloudFront-Is-Mobile-Viewer': [
+                    'false'
+                ],
+                'CloudFront-Is-SmartTV-Viewer': [
+                    'false'
+                ],
+                'CloudFront-Is-Tablet-Viewer': [
+                    'false'
+                ],
+                'CloudFront-Viewer-Country': [
+                    'GB'
+                ],
+                'Host': [
+                    't5nynu5k43.execute-api.eu-west-1.amazonaws.com'
+                ],
+                'Postman-Token': [
+                    '9b5ee25b-95ad-4a89-93ef-711fabbda104'
+                ],
+                'User-Agent': [
+                    'PostmanRuntime/7.28.0'
+                ],
+                'Via': [
+                    '1.1 1064639c622430d6b0382968293fd56e.cloudfront.net (CloudFront)'
+                ],
+                'X-Amz-Cf-Id': [
+                    '-hwcXYIM6mTExEuibdTST-S2nOe_FVKZQWVM32_p4u8OxqW7nj8h_g=='
+                ],
+                'X-Amzn-Trace-Id': [
+                    'Root=1-6092cfea-75dc6e145a8d260d55b45e61'
+                ],
+                'X-Forwarded-For': [
+                    '81.153.217.128, 130.176.96.147'
+                ],
+                'X-Forwarded-Port': [
+                    '443'
+                ],
+                'X-Forwarded-Proto': [
+                    'https'
+                ]
+            },
+            'QueryStringParameters': null,
+            'MultiValueQueryStringParameters': null,
+            'PathParameters': {
+                'proxy': 'api/healthcheck'
+            },
+            'StageVariables': null,
+            'RequestContext': {
+                'Path': '/Prod/api/healthcheck',
+                'AccountId': '406726073663',
+                'ResourceId': 'mr94pg',
+                'Stage': 'Prod',
+                'RequestId': 'e530e988-3e35-4fbf-b8cc-5890dc691271',
+                'Identity': {
+                    'CognitoIdentityPoolId': null,
+                    'AccountId': null,
+                    'CognitoIdentityId': null,
+                    'Caller': null,
+                    'ApiKey': null,
+                    'ApiKeyId': null,
+                    'AccessKey': null,
+                    'SourceIp': '81.153.217.128',
+                    'CognitoAuthenticationType': null,
+                    'CognitoAuthenticationProvider': null,
+                    'UserArn': null,
+                    'UserAgent': 'PostmanRuntime/7.28.0',
+                    'User': null,
+                    'ClientCert': null
+                },
+                'ResourcePath': '/{proxy+}',
+                'HttpMethod': 'GET',
+                'ApiId': 't5nynu5k43',
+                'ExtendedRequestId': 'e3VspHnwjoEFcJQ=',
+                'ConnectionId': null,
+                'ConnectionAt': 0,
+                'DomainName': 't5nynu5k43.execute-api.eu-west-1.amazonaws.com',
+                'DomainPrefix': 't5nynu5k43',
+                'EventType': null,
+                'MessageId': null,
+                'RouteKey': null,
+                'Authorizer': null,
+                'OperationName': null,
+                'Error': null,
+                'IntegrationLatency': null,
+                'MessageDirection': null,
+                'RequestTime': '05/May/2021:17:03:38 +0000',
+                'RequestTimeEpoch': 1620234218473,
+                'Status': null
+            },
+            'Body': null,
+            'IsBase64Encoded': false
+        }";
+
+        public override async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest request, ILambdaContext lambdaContext)
+        {
+            Console.WriteLine("Request:");
+            Console.WriteLine(JsonConvert.SerializeObject(request));
+
+            if (request.Resource == "WarmingLambda")
+            {
+                Console.WriteLine("Warming in overridden FunctionHandlerAsync…");
+                Console.WriteLine($"LambdaTime : {LambdaTime.ToShortTimeString()}");
+
+                var concurrencyCount = 1;
+                int.TryParse(request.Body, out concurrencyCount);
+
+                //if (concurrencyCount > 1)
+                //{
+                //    Console.WriteLine($"Warming instance { concurrencyCount}.");
+                //    var client = new AmazonLambdaClient();
+                //    await client.InvokeAsync(new Amazon.Lambda.Model.InvokeRequest
+                //    {
+                //        FunctionName = lambdaContext.FunctionName,
+                //        InvocationType = InvocationType.RequestResponse,
+                //        Payload = JsonConvert.SerializeObject(new APIGatewayProxyRequest
+                //        {
+                //            Resource = request.Resource,
+                //            Body = (concurrencyCount - 1).ToString()
+                //        })
+                //    });
+                //}
+
+
+                var apiReq = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(realReq);
+
+                return await base.FunctionHandlerAsync(apiReq, lambdaContext);
+                //return new APIGatewayProxyResponse { };
+
+
+            }
+
+            return await base.FunctionHandlerAsync(request, lambdaContext);
         }
     }
 }
