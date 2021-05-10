d94ada143db2ea80a077135a85fa3f3a4d2e057d
Steve Townend <steve@townendmail.co.uk>
2021-05-10 20:31:09 +0100

    Added news

---
+++ AnglingClubWebServices/Controllers/NewsController.cs
@@ -0,0 +1,77 @@
+using AnglingClubWebServices.Interfaces;
+using AnglingClubWebServices.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace AnglingClubWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController : AnglingClubControllerBase
+    {
+        private readonly INewsRepository _newsRepository;
+        private readonly ILogger<NewsController> _logger;
+        private readonly IMapper _mapper;
+
+        public NewsController(
+            INewsRepository newsRepository,
+            IMapper mapper,
+            ILoggerFactory loggerFactory)
+        {
+            _newsRepository = newsRepository;
+            _mapper = mapper;
+            _logger = loggerFactory.CreateLogger<NewsController>();
+            base.Logger = _logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<NewsItem>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public IActionResult Get()
+        {
+            StartTimer();
+
+            var items = _newsRepository.GetNewsItems().Result;
+
+            ReportTimer("Getting news items");
+
+            return Ok(items);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]List<NewsItem> newsItems)
+        {
+            StartTimer();
+
+            foreach (var newsItem in newsItems)
+            {
+                _newsRepository.AddOrUpdateNewsItem(newsItem);
+            }
+
+            ReportTimer("Posting news items");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AnglingClubWebServices/Data/NewsRepository.cs
@@ -0,0 +1,118 @@
+﻿using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using AnglingClubWebServices.Interfaces;
+using AnglingClubWebServices.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnglingClubWebServices.Data
+{
+    public class NewsRepository : RepositoryBase, INewsRepository
+    {
+        private const string IdPrefix = "NewsItem";
+        private readonly ILogger<NewsRepository> _logger;
+
+        public NewsRepository(
+            IOptions<RepositoryOptions> opts,
+            ILoggerFactory loggerFactory) : base(opts.Value.AWSAccessId, opts.Value.AWSSecret, opts.Value.SimpleDbDomain, loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<NewsRepository>();
+        }
+
+        public async Task AddOrUpdateNewsItem(NewsItem newsItem)
+        {
+            var client = GetClient();
+
+            if (newsItem.DbKey == null)
+            {
+                newsItem.DbKey = newsItem.GenerateDbKey(IdPrefix);
+            }
+
+            BatchPutAttributesRequest request = new BatchPutAttributesRequest();
+            request.DomainName = Domain;
+
+            // Mandatory properties
+            var attributes = new List<ReplaceableAttribute>
+            {
+                new ReplaceableAttribute { Name = "Date", Value = dateToString(newsItem.Date), Replace = true },
+                new ReplaceableAttribute { Name = "Title", Value = newsItem.Title, Replace = true },
+                new ReplaceableAttribute { Name = "Body", Value = newsItem.Body, Replace = true },
+
+            };
+
+            request.Items.Add(
+                new ReplaceableItem
+                {
+                    Name = newsItem.DbKey,
+                    Attributes = attributes
+                }
+            ); 
+
+            try
+            {
+                BatchPutAttributesResponse response = await client.BatchPutAttributesAsync(request);
+                _logger.LogDebug($"News Item added: {newsItem.DbKey} - {newsItem.Title}");
+            }
+            catch (AmazonSimpleDBException ex)
+            {
+                _logger.LogError(ex, $"Error Code: {ex.ErrorCode}, Error Type: {ex.ErrorType}");
+                throw;
+            }
+
+        }
+
+        public async Task<List<NewsItem>> GetNewsItems()
+        {
+            _logger.LogWarning($"Getting news items at : {DateTime.Now.ToString("HH:mm:ss.000")}");
+
+            var newsItems = new List<NewsItem>();
+
+            var client = GetClient();
+
+            SelectRequest request = new SelectRequest();
+            request.SelectExpression = $"SELECT * FROM {Domain} WHERE ItemName() LIKE '{IdPrefix}:%' AND Date > '' ORDER BY Date DESC";
+
+            SelectResponse response = await client.SelectAsync(request);
+
+            foreach (var item in response.Items)
+            {
+                var newsItem = new NewsItem();
+
+                newsItem.DbKey = item.Name;
+
+                foreach (var attribute in item.Attributes)
+                {
+                    switch (attribute.Name)
+                    {
+                        case "Date":
+                            newsItem.Date = DateTime.Parse(attribute.Value);
+                            break;
+
+
+                        case "Title":
+                            newsItem.Title = attribute.Value;
+                            break;
+
+                        case "Body":
+                            newsItem.Body = attribute.Value;
+                            break;
+
+
+                        default:
+                            break;
+                    }
+                }
+
+                newsItems.Add(newsItem);
+            }
+
+            return newsItems;
+
+        }
+
+
+    }
+}
