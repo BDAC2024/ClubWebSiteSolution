ac2de31748eba7ee70ed7d752a2c98e55f421d55
Steve Townend <steve@townendmail.co.uk>
2022-07-12 12:06:36 +0100

    Sorted OSU results sort order

--- AnglingClubWebServices/Interfaces/IMatchResultService.cs
+++ AnglingClubWebServices/Interfaces/IMatchResultService.cs
@@ -5,7 +5,7 @@ namespace AnglingClubWebServices.Interfaces
 {
     public interface IMatchResultService
     {
-        List<MatchResult> GetResults(string matchId);
+        List<MatchResult> GetResults(string matchId, MatchType matchType);
         List<LeaguePosition> GetLeagueStandings(MatchType matchType, Season season);
         List<AggregateWeight> GetAggregateWeights(AggregateWeightType aggWeightType, Season season);
     }
