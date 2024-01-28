02d579b1ccf9775763806a15da27cf883a268421
smtpagesuser <townendsteve+smtpagesuser@gmail.com>
2024-01-28 15:09:45 +0000

    QuestPDF now used for tickets

--- AnglingClubWebServices/Interfaces/ITicketService.cs
+++ AnglingClubWebServices/Interfaces/ITicketService.cs
@@ -5,6 +5,6 @@ namespace AnglingClubWebServices.Interfaces
     public interface ITicketService
     {
         void IssueDayTicket(DateTime validOn, string holdersName, string emailAddress, string paymentId);
-        void IssueGuestTicket(DateTime validOn, string membersName, string guestsName, int membershipNumber, string emailAddress, string paymentId);
+        void IssueGuestTicket(DateTime validOn, DateTime issuedOn, string membersName, string guestsName, int membershipNumber, string emailAddress, string paymentId);
     }
 }
