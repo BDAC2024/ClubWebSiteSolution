baa4599edfdb985850319acdb7d50475e9f1e561
Steve Townend <steve@townendmail.co.uk>
2024-09-06 10:47:34 +0100

    Now uses mailjet API

--- AnglingClubWebServices/Services/EmailService.cs
+++ AnglingClubWebServices/Services/EmailService.cs
@@ -1,15 +1,20 @@
 ﻿using AnglingClubWebServices.Interfaces;
 using AnglingClubWebServices.Models;
+using Mailjet.Client;
+using Mailjet.Client.Resources;
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using MimeKit.Encodings;
+using Newtonsoft.Json.Linq;
 using Org.BouncyCastle.Cms;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Threading.Tasks;
 
 namespace AnglingClubWebServices.Services
 {
@@ -42,63 +47,78 @@ namespace AnglingClubWebServices.Services
 
         public void SendEmail(List<string> to, string subject, string textBody, List<string> attachmentFilenames = null, List<ImageAttachment> imageAttachments = null, List<StreamAttachment> streamAttachments = null)
         {
-            if (to.Any())
+            if (_options.UseEmailAPI)
             {
-                var mailMessage = new MimeMessage();
-                mailMessage.From.Add(new MailboxAddress(_options.PrimaryEmailFromName, _options.PrimaryEmailFromAddress));
-                foreach (var recipient in to)
+                sendViaApi(to, subject, textBody, attachmentFilenames, imageAttachments, streamAttachments).Wait();
+            }
+            else
+            {
+                if (to.Any())
                 {
-                    mailMessage.To.Add(MailboxAddress.Parse(recipient));
-                }
-
-                if (!string.IsNullOrEmpty(_options.PrimaryEmailBCC))
-                {
-                    mailMessage.Bcc.Add(MailboxAddress.Parse(_options.PrimaryEmailBCC));
-                }
-
-                mailMessage.Subject = subject;
-
-                var builder = new BodyBuilder();
-                builder.TextBody = textBody;
-                builder.HtmlBody = textBody + "<br><br>";
-
-                if (imageAttachments != null)
-                {
-                    foreach (var att in imageAttachments)
+                    var mailMessage = new MimeMessage();
+                    mailMessage.From.Add(new MailboxAddress(_options.PrimaryEmailFromName, _options.PrimaryEmailFromAddress));
+                    foreach (var recipient in to)
                     {
-                        var imageDataUrl = att.DataUrl; 
-                        var imageBase64 = imageDataUrl.Replace("data:image/png;base64,", "");
-                        byte[] temp_backToBytes = Convert.FromBase64String(imageBase64);
-                        var imageAtt = new MimePart("image", "png", new MemoryStream(temp_backToBytes))
+                        mailMessage.To.Add(MailboxAddress.Parse(recipient));
+                    }
+
+                    if (!string.IsNullOrEmpty(_options.PrimaryEmailBCC))
+                    {
+                        mailMessage.Bcc.Add(MailboxAddress.Parse(_options.PrimaryEmailBCC));
+                    }
+
+                    mailMessage.Subject = subject;
+
+                    var builder = new BodyBuilder();
+                    builder.TextBody = textBody;
+                    builder.HtmlBody = textBody + "<br><br>";
+
+                    if (imageAttachments != null)
+                    {
+                        foreach (var att in imageAttachments)
                         {
-                            ContentTransferEncoding = ContentEncoding.Base64,
-                            FileName = att.Filename
-                        };
+                            var imageDataUrl = att.DataUrl;
+                            var imageBase64 = imageDataUrl.Replace("data:image/png;base64,", "");
+                            byte[] temp_backToBytes = Convert.FromBase64String(imageBase64);
+                            var imageAtt = new MimePart("image", "png", new MemoryStream(temp_backToBytes))
+                            {
+                                ContentTransferEncoding = ContentEncoding.Base64,
+                                FileName = att.Filename
+                            };
 
-                        builder.Attachments.Add(imageAtt);
+                            builder.Attachments.Add(imageAtt);
 
-                        //builder.HtmlBody += $"<img src='{att.DataUrl}'/>";
+                            //builder.HtmlBody += $"<img src='{att.DataUrl}'/>";
+                        }
                     }
-                }
 
-                if (attachmentFilenames != null)
-                {
-                    foreach (var att in attachmentFilenames)
+                    if (attachmentFilenames != null)
                     {
-                        builder.Attachments.Add(att);
+                        foreach (var att in attachmentFilenames)
+                        {
+                            builder.Attachments.Add(att);
+                        }
                     }
-                }
 
-                if (streamAttachments != null)
-                {
-                    foreach (var att in streamAttachments)
+                    if (streamAttachments != null)
                     {
-                        builder.Attachments.Add(att.Filename, att.Bytes, new MimeKit.ContentType(att.ContentType, ""));
+                        foreach (var att in streamAttachments)
+                        {
+                            builder.Attachments.Add(att.Filename, att.Bytes, new MimeKit.ContentType(att.ContentType, ""));
+                        }
                     }
-                }
-                mailMessage.Body = builder.ToMessageBody();
+                    mailMessage.Body = builder.ToMessageBody();
 
-                sendWithFallback(mailMessage);
+                    // No longer used
+                    //sendWithFallback(mailMessage);
+
+                    // Mailjet SMTP timing out on AWS Lambda so using the fallback (outlook) settings. Although these will be removed soon.
+                    mailMessage.From.Remove(new MailboxAddress(_options.PrimaryEmailFromName, _options.PrimaryEmailFromAddress));
+                    mailMessage.From.Add(new MailboxAddress(_options.FallbackEmailFromName, _options.FallbackEmailFromAddress));
+
+                    sendViaSMTP(mailMessage, _options.FallbackEmailHost, _options.FallbackEmailPort, _options.FallbackEmailUsername, _options.FallbackEmailPassword);
+
+                }
             }
         }
 
@@ -156,6 +176,99 @@ namespace AnglingClubWebServices.Services
                 }
             }
 
+        }
+
+        private async Task sendViaApi(List<string> to, string subject, string textBody, List<string> attachmentFilenames = null, List<ImageAttachment> imageAttachments = null, List<StreamAttachment> streamAttachments = null)
+        {
+            var toArray = new JArray();
+            foreach (var recip in to)
+            {
+                toArray.Add(JObject.Parse
+                (
+                    @"{""Email"":""" + recip + "\", " +
+                    @"""Name"":""" + recip +
+                @"""}"));
+            }
+
+            var bccArray = new JArray();
+            if (!string.IsNullOrEmpty(_options.PrimaryEmailBCC))
+            {
+                bccArray.Add(JObject.Parse
+                (
+                    @"{""Email"":""" + _options.PrimaryEmailBCC + "\", " +
+                    @"""Name"":""" + _options.PrimaryEmailBCC +
+                @"""}"));
+            }
+
+            var attStreamArray = new JArray();
+            if (streamAttachments != null)
+            {
+                foreach (var att in streamAttachments)
+                {
+                    attStreamArray.Add(JObject.Parse
+                    (
+                        @"{""ContentType"":""" + att.ContentType + "\", " +
+                        @"""Filename"":""" + att.Filename + "\", " +
+                        @"""Base64Content"":""" + Convert.ToBase64String(att.Bytes) +
+                    @"""}"));
+                }
+            }
+
+            if (imageAttachments != null)
+            {
+                foreach (var att in imageAttachments)
+                {
+                    var imageDataUrl = att.DataUrl;
+                    var imageBase64 = imageDataUrl.Replace("data:image/png;base64,", "");
+
+                    attStreamArray.Add(JObject.Parse
+                    (
+                        @"{""ContentType"":""" + "data:image/png" + "\", " +
+                        @"""Filename"":""" + att.Filename + "\", " +
+                        @"""Base64Content"":""" + imageBase64 +
+                    @"""}"));
+
+                }
+            }
+
+            MailjetClient client = new MailjetClient(_options.EmailAPIPublicKey, _options.EmailAPIPrivateKey);
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = SendV31.Resource
+            }
+            .Property(Send.Messages, new JArray 
+            {
+                new JObject 
+                {
+                    { "From", new JObject 
+                        {
+                            {"Email", _options.PrimaryEmailFromAddress},
+                            {"Name", _options.PrimaryEmailFromName}
+                        }
+                    },
+                    {"To", toArray},
+                    {"Bcc", bccArray},
+                    {"Subject", subject},
+                    //{"TextPart", ""},
+                    {"HTMLPart", textBody},
+                    {"Attachments", attStreamArray }
+                }
+            })
+            .Property(Send.SandboxMode, false); 
+
+            MailjetResponse response = await client.PostAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation($"Email sent via API to: {to.First()}");
+                //_logger.LogWarning(response.GetData());
+            }
+            else
+            {
+                _logger.LogError($"Email failed via API to: {to.First()}");
+                _logger.LogError(string.Format("Email StatusCode: {0}\n", response.StatusCode));
+                _logger.LogError(string.Format("Email ErrorInfo: {0}\n", response.GetErrorInfo()));
+                _logger.LogError(string.Format("Email ErrorMessage: {0}\n", response.GetErrorMessage()));
+            }
         }
 
         private void sendViaSMTP(MimeMessage mailMessage, string emailHost, int emailPort, string emailUsername, string emailPassword)
