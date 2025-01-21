07cb943e2eb98f7f5f037e1e39462d8cf7638b08
Steve Townend <steve@townendmail.co.uk>
2025-01-21 16:04:08 +0000

    Mvvm added

---
+++ AnglingClubWebsite/MainLayout.ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AnglingClubWebsite.SharedComponents;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AnglingClubWebsite
+{
+    public partial class MainLayoutViewModel : ViewModelBase
+    {
+        public MainLayoutViewModel(
+            IMessenger messenger) : base(messenger)
+        {
+            
+        }
+
+        [ObservableProperty]
+        private string _testMessage = "Hello from MainLayoutViewModel";
+
+    }
+}
---
+++ AnglingClubWebsite/SharedComponents/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AnglingClubWebsite.SharedComponents
+{
+    public abstract partial class ViewModelBase : ObservableObject, IViewModelBase
+    {
+        private readonly IMessenger _messenger;
+
+        protected ViewModelBase(IMessenger messenger)
+        {
+            _messenger = messenger;
+        }
+
+        protected virtual void NotifyStatChanged() => OnPropertyChanged((string?)null);
+
+        public virtual async Task OnInitializedAsync()
+        {
+            await Loaded().ConfigureAwait(true);
+        }
+
+        [RelayCommand]
+        public virtual async Task Loaded()
+        {
+
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+
+    }
+
+}
