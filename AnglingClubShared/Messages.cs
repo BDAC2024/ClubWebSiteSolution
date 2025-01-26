a6a0000bf7f54bf8eb15be7ebe5ae57e6afe70bf
Steve Townend <steve@townendmail.co.uk>
2025-01-26 12:11:42 +0000

    AppDialog now showing

--- AnglingClubShared/Messages.cs
+++ AnglingClubShared/Messages.cs
@@ -12,6 +12,8 @@ namespace AnglingClubShared
     public record class ShowProgress();
     public record class HideProgress();
 
+    public record class ShowConsoleMessage(string Content);
+
     public record class ShowMessage(MessageState State, string Title, string Body, MessageButton? confirmationButtonDetails = null);
 
     public record MessageButton()
