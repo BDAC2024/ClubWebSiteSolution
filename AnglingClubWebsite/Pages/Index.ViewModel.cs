23a1427010f1da23a6b0780eebcc586d7bf7ee02
Steve Townend <steve@townendmail.co.uk>
2025-01-27 12:02:48 +0000

    Welcome page done

--- AnglingClubWebsite/Pages/Index.ViewModel.cs
+++ AnglingClubWebsite/Pages/Index.ViewModel.cs
@@ -1,3 +1,23 @@
-﻿namespace AnglingClubWebsite.Pages
+﻿using AnglingClubShared.DTOs;
+using AnglingClubWebsite.Services;
+using AnglingClubWebsite.SharedComponents;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AnglingClubWebsite.Pages
 {
+    public partial class IndexViewModel : ViewModelBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IMessenger _messenger;
+
+        public IndexViewModel(
+            IAuthenticationService authenticationService,
+            IMessenger messenger,
+            ICurrentUserService currentUserService) : base(messenger, currentUserService)
+        {
+            _authenticationService = authenticationService;
+            _messenger = messenger;
+        }
+    }
 }
