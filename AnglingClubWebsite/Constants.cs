610e50b6466f3c7917df0c195224462ce4329bf1
Steve Townend <steve@townendmail.co.uk>
2025-02-06 16:35:06 +0000

    Maps now working. Still need routes adding.

--- AnglingClubWebsite/Constants.cs
+++ AnglingClubWebsite/Constants.cs
@@ -3,6 +3,7 @@
     public static class Constants
     {
         public const string VIDEO_BASE_URL = "https://www.youtube.com/embed";
+        public const string MAP_DIRECTIONS_BASE_URL = "https://www.google.co.uk/maps/dir";
 
         public const string AUTH_KEY = "currentMember";
         public const string HTTP_CLIENT_KEY = "ServerApi";
