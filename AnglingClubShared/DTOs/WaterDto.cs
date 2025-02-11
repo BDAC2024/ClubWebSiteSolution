08a9429b4e09915d073787a8731c9f43f3eef714
Steve Townend <steve@townendmail.co.uk>
2025-02-11 19:41:07 +0000

    More water debuging

--- AnglingClubShared/DTOs/WaterDto.cs
+++ AnglingClubShared/DTOs/WaterDto.cs
@@ -9,19 +9,19 @@ namespace AnglingClubShared.DTOs
     public class WaterInputDto : WaterBase
     {
         public float Id { get; set; }
-        public List<string> MarkerIcons { get; set; }
-        public List<string> MarkerLabels { get; set; }
+        public List<string> MarkerIcons { get; set; } = new List<string>();
+        public List<string> MarkerLabels { get; set; } = new List<string>();
 
-        public List<double> Destination { get; set; }
-        public List<double> Centre { get; set; }
-        public List<double> Markers { get; set; }
-        public List<double> Path { get; set; }
+        public List<double> Destination { get; set; } = new List<double>();
+        public List<double> Centre { get; set; } = new List<double>();
+        public List<double> Markers { get; set; } = new List<double>();
+        public List<double> Path { get; set; } = new List<double>();
     }
 
     public class WaterUpdateDto : TableBase
     {
-        public string Description { get; set; }
-        public string Directions { get; set; }
+        public string Description { get; set; } = "";
+        public string Directions { get; set; } = "";
     }
 
     public class WaterOutputDto : WaterBase
@@ -44,8 +44,8 @@ namespace AnglingClubShared.DTOs
 
         public List<Marker> Markers { get; set; } = new List<Marker>();
 
-        public Position Centre { get; set; }
-        public Position Destination { get; set; }
+        public Position Centre { get; set; } = new Position();    
+        public Position Destination { get; set; } = new Position();
         public List<Position> Path { get; set; } = new List<Position>();
 
         public bool HasLimits 
