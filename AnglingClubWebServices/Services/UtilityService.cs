852c88dac2319498e14648df74e1b723353c446e
Steve Townend <steve@townendmail.co.uk>
2025-04-04 16:00:31 +0100

    Improved book printing

--- AnglingClubWebServices/Services/UtilityService.cs
+++ AnglingClubWebServices/Services/UtilityService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System;
 using AnglingClubWebServices.Interfaces;
+using System.Linq;
 
 namespace AnglingClubWebServices.Services
 {
@@ -16,114 +17,64 @@ namespace AnglingClubWebServices.Services
         /// to ease the production of membership books/diaries.
         /// </summary>
         /// <param name="numPages">Number of real pages in your document, not including any padding pages at teh end.</param>
-        /// <param name="pagesPerSide">The number of pages to print per side</param>
+        /// <param name="pagesPerSheet">The number of pages (per book) to print per sheet of paper</param>
         /// <param name="printCoverSeparately">Whether to produce 2 lists of pages; one for the front/back cover and one for the rest of the content</param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        public List<PrintPaginationSummary> CalcPagesForBookPrinting(int numPages, int pagesPerSide, bool printCoverSeparately)
+        public List<PrintPaginationSummary> CalcPagesForBookPrinting(int numPages, int pagesPerSheet, bool printCoverSeparately)
         {
-            var doubleSided = true;
-
             if (numPages % 4 != 0)
             {
                 throw new Exception("Sorry; number of pages must be divisible by 4 - aborted");
             }
 
-            var pagesPerSheet = pagesPerSide * (doubleSided ? 2 : 1);
-            var dummyCount = numPages % pagesPerSheet;
-            var sheetsRequired = (numPages + dummyCount) / pagesPerSheet;
-
-            var fp = 1;
-            var lp = numPages;
-
-            var row1Pages = new string[sheetsRequired * 2];
-            var row2Pages = new string[sheetsRequired * 2];
-
-            var row11CreationIndex = 0;
-            var row2CreationIndex = 0;
-
-            // Row 1 first
-            for (int sheet = 0; sheet < sheetsRequired; sheet++)
-            {
-                for (int side = 0; side < 2; side++)
-                {
-                    string v = "";
-                    if (side == 0)
-                    {
-                        v = $"{lp--},{fp++},";
-                    }
-                    if (side == 1)
-                    {
-                        v = $"{fp++},{lp--},";
-                    }
-                    row1Pages[row11CreationIndex++] = v;
-                    debugPrint($"Row2 : sheet{sheet}, side{side} = {v}");
-                }
-            }
-
-            // Row 2 next
-            for (int sheet = 0; sheet < sheetsRequired; sheet++)
-            {
-                for (int side = 0; side < 2; side++)
-                {
-                    string v = "";
-
-                    if (side == 0)
-                    {
-                        v = $"{lp--},{fp++},";
-                    }
-                    if (side == 1)
-                    {
-                        v = $"{fp++},{lp--},";
-                    }
-                    row2Pages[row2CreationIndex++] = v;
-                    debugPrint($"Row1 : sheet{sheet}, side{side} = {v}");
-                }
-            }
-
-            // Add dummy/blank pages if required
-            if (dummyCount > 0 || (dummyCount == 0 && printCoverSeparately))
-            {
-                row2Pages[sheetsRequired * 2 - 1] = $"{numPages + 1},{numPages + 1},";
-                row2Pages[sheetsRequired * 2 - 2] = $"{numPages + 1},{numPages + 1},";
-            }
-
             List<PrintPaginationSummary> printPages = new List<PrintPaginationSummary>();
 
-            var row11Index = 0;
-            var row2Index = 0;
+            int numSheets = numPages / pagesPerSheet;
 
+            List<int> pageNumbers = new List<int>();
+
+            for (var sheet = 0; sheet < numSheets; sheet++)
+            {
+                var frontLeft = numPages - sheet * (pagesPerSheet / 2);
+                var frontRight = 1 + sheet * (pagesPerSheet / 2);
+
+                pageNumbers.Add(frontLeft);
+                pageNumbers.Add(frontRight);
+                pageNumbers.Add(frontLeft);
+                pageNumbers.Add(frontRight);
+
+                var backLeft = frontRight + 1;
+                var backRight = frontLeft - 1;
+
+                pageNumbers.Add(backLeft);
+                pageNumbers.Add(backRight);
+                pageNumbers.Add(backLeft);
+                pageNumbers.Add(backRight);
+
+            }
+
+            int innerContentStart = 0;
             if (printCoverSeparately)
             {
+                innerContentStart = pagesPerSheet * 2;
+
                 PrintPaginationSummary coverPage = new PrintPaginationSummary();
-
-                for (int side = 0; side < 2; side++)
-                {
-                    coverPage.PagesToPrint += $"{row1Pages[row11Index]}{row1Pages[row11Index]}";
-                    row11Index++;
-                }
-
-                coverPage.Instructions = "For the front cover; Under Settings choose 'Custom Print' then enter the following page numbers: -";
-                coverPage.PagesToPrint = (coverPage.PagesToPrint + coverPage.PagesToPrint).Substring(0, coverPage.PagesToPrint.Length - 1); // drop the last comma
+                coverPage.Instructions = "Under Settings choose 'Custom Print' then enter the following page numbers: -";
+                coverPage.PagesToPrint = string.Join(",", pageNumbers.Take(innerContentStart).ToArray());
                 printPages.Add(coverPage);
+
+                debugPrint("Cover:");
+                debugPrint(string.Join(",", pageNumbers.Take(innerContentStart).ToArray()));
             }
 
-            PrintPaginationSummary summary = new PrintPaginationSummary();
+            PrintPaginationSummary pages = new PrintPaginationSummary();
+            pages.Instructions += "Under Settings choose 'Custom Print' then enter the following page numbers: -";
+            pages.PagesToPrint = string.Join(",", pageNumbers.Skip(innerContentStart).ToArray());
+            printPages.Add(pages);
 
-            row11Index = printCoverSeparately ? 2 : 0;
-            row2Index = 0;
-
-            var loopCount = printCoverSeparately ? row2Pages.Length - 2 : row2Pages.Length;
-
-            for (int i = 0; i < loopCount; i++)
-            {
-                summary.PagesToPrint += $"{row1Pages[row11Index++]}{row2Pages[row2Index++]}";
-            }
-
-            summary.Instructions = printCoverSeparately ? "For the inside pages; " : "";
-            summary.Instructions += "Under Settings choose 'Custom Print' then enter the following page numbers: -";
-            summary.PagesToPrint = summary.PagesToPrint.Substring(0, summary.PagesToPrint.Length - 1); // drop the last comma
-            printPages.Add(summary);
+            debugPrint("Content:");
+            debugPrint(string.Join(",", pageNumbers.Skip(innerContentStart).ToArray()));
 
             return printPages;
         }
