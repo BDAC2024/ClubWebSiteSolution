4541f48f8c4772c66f1daa6b9ac37171ac6a1d64
Steve Townend <steve@townendmail.co.uk>
2025-11-06 12:05:58 +0000

    Use presignedURL for uploads

---
+++ AnglingClubWebServices/Controllers/TmpFileController.cs
@@ -0,0 +1,53 @@
+using AnglingClubWebServices.DTOs;
+using AnglingClubWebServices.Interfaces;
+using AnglingClubWebServices.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AnglingClubWebServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class TmpFileController : ControllerBase
+    {
+        private readonly ILogger<TmpFileController> _logger;
+        private readonly ITmpFileRepository _referenceDataRepository;
+        private readonly ITmpFileRepository _tmpFileRepository;
+        private readonly IMapper _mapper;
+
+        public TmpFileController(
+            IMapper mapper,
+            ILoggerFactory loggerFactory,
+            ITmpFileRepository tmpFileRepository)
+        {
+            _mapper = mapper;
+            _logger = loggerFactory.CreateLogger<TmpFileController>();
+            _tmpFileRepository = tmpFileRepository;
+        }
+
+        /// <summary>
+        /// Initial attempts to upload the file as an arg to a web api call failed on AWS with a 413 (content too large) error.
+        /// The approach here is to get a pre-signed URL from the web api, then use that URL to upload the file directly to S3.
+        /// The solution was obtained from ChatGPT
+        /// </summary>
+        /// <param name="tmpFileUploadUrlDto"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost("GetUploadUrl")]
+        public async Task<IActionResult> GetUploadUrl([FromBody] TmpFileUploadUrlDto tmpFileUploadUrlDto)
+        {
+            var fileId = Guid.NewGuid().ToString();
+
+            var url = await _tmpFileRepository.GetTmpFileUploadUrl(fileId, tmpFileUploadUrlDto.ContentType);
+
+
+            return Ok(new TmpFileUploadUrlResult { UploadUrl = url, UploadedFileName = fileId });
+        }
+
+    }
+}
--- AnglingClubWebServices/Interfaces/ITmpFileRepository.cs
+++ AnglingClubWebServices/Interfaces/ITmpFileRepository.cs
@@ -6,9 +6,10 @@ namespace AnglingClubWebServices.Interfaces
 {
     public interface ITmpFileRepository
     {
-        Task AddOrUpdateTmpFile(TmpFile file);
-        Task<List<TmpFile>> GetTmpFiles(bool loadFile = true);
-        Task<TmpFile> GetTmpFile(string id);
+        Task AddOrUpdateTmpFile(StoredFileMeta file);
+        Task<List<StoredFile>> GetTmpFiles(bool loadFile = false);
+        Task<StoredFile> GetTmpFile(string id);
         Task DeleteTmpFile(string id, bool deleteFromS3 = true);
+        Task<string> GetTmpFileUploadUrl(string filename, string contentType);
     }
 }
