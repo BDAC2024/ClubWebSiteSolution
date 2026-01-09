741953d25c305e275a22e10a376233a2cc5da794
Steve Townend <steve@townendmail.co.uk>
2026-01-09 17:09:53 +0000

    Added watermark to warn of dev/staging being used

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from './services/global.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,33 @@ export class AppComponent {
 
   lat = 54.091339;
   long = -1.355844;
+
+  runningOn: string = "";
+  envMessage: string = "";
+
+  constructor(
+    private globalService: GlobalService,
+
+  ) {
+    this.runningOn = globalService.RunningOn;
+
+    switch (this.runningOn.toLowerCase()) {
+      case "localhost":
+        this.envMessage = "LOCAL DEV / NOT LIVE";
+        break;
+
+      case "staging":
+        this.envMessage = "STAGING / NOT LIVE";
+        break;
+
+      case "devtunnel":
+        this.envMessage = "DEVTUNNEL / NOT LIVE";
+        break;
+
+      default:
+        this.envMessage = "";
+    }
+  }
+
+  
 }
